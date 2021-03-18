049c8b22a66655596175c9a1e8b1b229988d00d1
FatimaAlShraa <falhmood66@gmail.com>
2021-03-18 15:30:19 +0200

    add JS

---
+++ JavaScript/app.js
@@ -0,0 +1,27 @@
+var doWork="done by me";
+
+document.write('<h3>'+doWork+'</h3>');
+
+var universityName=prompt("what is name of university ?")
+
+// var student=JUST;
+
+if(universityName==='JUST')
+{document.write('<img src="OIP.jpg"/>');
+
+}  else
+{ document.write('<img src="jo.jpg"/>');
+}
+
+var age=prompt("what is your age?");
+if(age>= 15){
+    alert('go on and learn');
+    if(age<15){
+        alert('go on too')
+    }
+}
+
+var bored=true;
+if(bored==true){
+    console.log('go to sleep');
+}
