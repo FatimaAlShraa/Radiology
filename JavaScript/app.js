cb09a56932d011356afbb218ec32eac5d5a0cc1d
FatimaAlShraa <falhmood66@gmail.com>
2021-03-21 14:05:51 +0200

    do while and for

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,27 +1,54 @@
-var doWork="done by me";
+var doWork = "done by me";
 
-document.write('<h3>'+doWork+'</h3>');
+document.write('<h3>' + doWork + '</h3>');
 
-var universityName=prompt("what is name of university ?")
+var universityName = prompt("what is name of university ?")
 
 // var student=JUST;
-
-if(universityName==='JUST')
-{document.write('<img src="OIP.jpg"/>');
-
-}  else
-{ document.write('<img src="jo.jpg"/>');
+while (universityName !== 'JUST' && universityName !== 'Jordand University') {
+    universityName = prompt('please only write JUST / Jordan University');
 }
 
-var age=prompt("what is your age?");
-if(age>= 15){
-    alert('go on and learn');
-    if(age<15){
+var numberOFUniversity = prompt('How much university you want?');
+
+
+
+
+var university = '';
+var result = '';
+
+if (universityName === 'JUST') {
+    university = ('<img src="OIP.jpg"/>');
+
+} else if (universityName == 'Joradn University') {
+    university = ('<img src="jo.jpg"/>');
+} else {
+    alert('choose between JUST and Jordan University');
+
+}
+for (var j = 0; j < numberOFUniversity; j++) {
+    result += university;
+}
+
+
+document.write(result)
+
+
+
+var age = prompt("what is your age?");
+
+console.log(age);
+
+if (age >= 15 && age < 45) {
+    {
+        alert('go on and learn');
+    }
+    if (age < 15 || age > 45) {
         alert('go on too')
     }
 }
 
-var bored=true;
-if(bored==true){
+var bored = true;
+if (bored == true) {
     console.log('go to sleep');
 }
